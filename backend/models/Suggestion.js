67a3a14e485ca70960af853b7902a2764a62cf55
Lilah Durney <ldurney@berkeley.edu>
2025-06-30 14:24:52 -0700

    frontend setup, input forms

--- backend/models/Suggestion.js
+++ backend/models/Suggestion.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
-const {Schema, models} = mongoose
+const { Schema, models } = mongoose;
 
 const SuggestionSchema = new Schema({
-  type: { type: String, required: true }, // "genAI" or "non-genAI"
-  suggestionId: { type: String, required: true }, // UUID or custom
-  design_brief: { type: String},
-  goal_category: { type: String},
-  semantic_distance: { type: String, enum: ["near", "medium", "far"]},
-  visual_similarity: { type: String, enum: ["low", "high"]},
-  conceptual_similarity: { type: String, enum: ["low", "high"] },
-  sketch_url: { type: String }, // optional
+  type: { type: String, required: true }, // "genAI" or "structured"
+  suggestionId: { type: String, required: true }, // UUID
+  designBrief: { type: String }, // design brief from user
+  goalCategory: { type: String }, // sustainability goal
+  semanticDistance: { type: String, enum: ["near", "medium", "far"] }, // might want to change this to a number scale instead (same for the similarities below)
+  visualSimilarity: { type: String, enum: ["low", "high"] },
+  conceptualSimilarity: { type: String, enum: ["low", "high"] },
+  sketchUrl: { type: String }, //optional --> would this actually be the actualy image? in terms of object storage not sure yet
   output: {
-    guideline: String, // for non-genAI
-    suggestion: String, // for genAI
+    guideline: String, //for structured
+    suggestion: String, //for genAI
     rationale: String,
     category: String,
-    source_guideline: String, // for genAI
-    similarity_score: Number // for non-genAI
+    sourceGuideline: String, // for genAI
+    similarityScore: Number, // for structured
   },
-  energy_used: { type: Number },
-  createdAt: { type: Date, default: Date.now }
+  energyUsed: { type: Number },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Suggestion = models.Suggestion || mongoose.model("Suggestion", SuggestionSchema);
+const Suggestion =
+  models.Suggestion || mongoose.model("Suggestion", SuggestionSchema);
 export default Suggestion;
